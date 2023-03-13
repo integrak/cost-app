5489e943380b13db6a68eca8c6518b3e5b1ba86b
integrak <integra_k@ukr.net>
2023-03-13 15:04:38 +0200

    deploy

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React, {useState} from "react";
+import NewCost from "./components/NewCost/NewCost";
+import Costs from "./components/Costs/Costs";
+
+const INITIAL_COSTS = [
+  {
+    id: 'c1',
+    date: new Date(2021, 2, 12),
+    description: "fridge",
+    amount: 999.99,
+  },
+  {
+    id: 'c2',
+    date: new Date(2021, 11, 25),
+    description: "MacBook",
+    amount: 1254.72,
+  },
+  {
+    id: 'c3',
+    date: new Date(2021, 4, 1),
+    description: "jeans",
+    amount: 49.99,
+  },
+];
+
+const App = () => {
+  
+  const [costs, setCosts] = useState(INITIAL_COSTS);
+
+  const addCostHandler = (cost) => {
+    setCosts((prevCosts) => {
+      console.log(cost);
+      return [cost, ...prevCosts];
+    });
+  };
+
+  return (
+  <div> 
+      <NewCost onAddCost={addCostHandler} />
+      <Costs costs={costs} />
+    </div>
+  );
+};
+
+export default App;
